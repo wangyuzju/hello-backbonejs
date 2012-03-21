62658825b33d2b5183fb6f55f90b532dfc413da5
Artur Adib <arturadib@gmail.com>
2012-03-21 09:02:13 -0400

    minor

--- 5.js
+++ 5.js
@@ -99,5 +99,5 @@
   var listView = new ListView();
 })(jQuery);
 
-// Liked it? <br/>
+// For more on coding and startups: <br/>
 // <a href="https://twitter.com/arturadib" class="twitter-follow-button" data-show-count="false">Follow @arturadib</a> <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
