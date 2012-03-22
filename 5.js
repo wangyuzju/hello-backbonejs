fbd83d02a12ef5d532242c7b16bd91f420700723
Artur Adib <arturadib@gmail.com>
2012-03-22 11:50:57 -0400

    added tracking

--- 5.js
+++ 5.js
@@ -99,5 +99,16 @@
   var listView = new ListView();
 })(jQuery);
 
-// For more on coding and startups: <br/>
-// <a href="https://twitter.com/arturadib" class="twitter-follow-button" data-show-count="false">Follow @arturadib</a> <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+// <p style="background:rgb(245,245,255); padding:10px;">I tweet about coding and startups: <a target="_blank" href="http://twitter.com/arturadib">@arturadib</a> </p>
+// <script>
+//   if (window.location.href.search(/\?x/) < 0) {
+//     var _gaq = _gaq || [];
+//     _gaq.push(['_setAccount', 'UA-924459-7']);
+//     _gaq.push(['_trackPageview']);
+//     (function() {
+//       var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+//       ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+//       var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+//     })();
+//   }
+// </script>
